811d1405b51da49b79a74c1253817e46c5c6752a
nstrong <nathanstrong.fx@gmail.com>
2021-08-07 17:35:10 -0700

    movie theatre

--- 02-movie-seat-booking/index.js
+++ 02-movie-seat-booking/index.js
@@ -4,7 +4,34 @@ const countEl = document.getElementById('count');
 const totalEl = document.getElementById('total');
 const movieSelectEl = document.getElementById('movie');
 
-const ticketPrice = movieSelectEl.value;
+let ticketPrice = movieSelectEl.value;
 
-console.log(Number(ticketPrice));
+containerEl.addEventListener('click', onSeatClick);
+movieSelectEl.addEventListener('change', onMovieSelect);
 
+function onSeatClick(e) {
+  if (!e.target.classList.contains('seat')) return;
+  if (e.target.classList.contains('occupied')) return;
+
+  const seatEl = e.target;
+  seatEl.classList.toggle('selected');
+
+  updateSelectedCount();
+}
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+  countEl.innerText = selectedSeatsCount;
+  totalEl.innerText = selectedSeatsCount * ticketPrice;
+}
+
+function onMovieSelect(e) {
+  ticketPrice = e.target.value;
+
+  totalEl.innerText = 0;
+  countEl.innerText = 0;
+
+  const seatsEl = document.querySelectorAll('.row .seat.selected');
+  seatsEl.forEach(seatEl => seatEl.classList.remove('selected'));
+}
